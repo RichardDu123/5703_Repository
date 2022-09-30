6fcc82893a6763fc9e533fbfe702281de6c8dcdc
donaldfjh <donaldfjh@gmail.com>
2022-09-30 17:50:41 +1000

    Profile page  and Statics page

--- client/src/views/profile/index.vue
+++ client/src/views/profile/index.vue
@@ -1,7 +1,181 @@
 <template>
-  <div class="profileContainer">Profile</div>
+  <div class="profileContainer">
+    <div class="navi">
+      My Profile
+    </div>
+
+
+  <div class="userInfo">
+
+    
+    <div class="p1">
+        <div class="icon">
+          <el-icon><User /></el-icon>
+        </div>
+
+        <div class="userName">
+          {{name}}
+        </div>
+    </div>
+
+    <div class="p2">
+        <div class="hint">
+          
+            Please fill your new nickname:
+          
+        </div>
+
+        <div class="newNike">
+          <text class="newSize">
+            New nikename:
+          </text>
+
+          <div class="editor">
+          <el-input v-model="input" placeholder="Please input" clearable />
+          </div>
+
+          
+        </div>
+        <div class="noMore">
+            No more than 10 charaters
+          </div>
+
+        
+    </div>
+
+    <div class="p3">
+        <div class="confirm">
+            <el-button @click="handleSubmit" round>Comfirm</el-button>
+        </div>
+
+        <div class="cancel">
+            <el-button type="success" round>Cancel</el-button>
+        </div>
+    </div>
+
+  </div>
+</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+let input = ref('')
+let name = ref('Tony')
+function handleSubmit(){
+  if(input.value.length>= 10){
+    alert('Cannot greater than 10 ')
+  }else{
+    name.value = input.value
+  }
+  
+}
 
-<style scoped lang="less"></style>
+// function handleCancel(){
+  
+// }
+</script>
+
+<style scoped lang="less">
+
+.profileContainer{
+
+}
+
+.userInfo{
+
+}
+
+.navi{
+  position: absolute;
+  font-family: Abel;
+  font-style: Regular;
+  font-size: 24px;
+}
+
+.p1{
+  position: absolute;
+  width: 100%;
+  top: 20%;
+  
+
+}
+
+.icon{
+  position: absolute;
+  left: 10%;
+}
+
+.userName{
+  position: absolute;
+  left: 12%;
+  font-weight: bold;
+}
+
+.p2{
+  position: absolute;
+  width: 100%;
+  height: 7%;
+  top: 25%;
+}
+
+.hint{
+  position: absolute;
+  left: 10%;
+  font-family: Abel;
+  font-size: 22px;
+}
+
+
+
+.newNike{
+ position: absolute;
+    top: 100%;
+    left: 10%;
+    width: 100%;
+    height: 50%;
+    display: flex;
+    
+}
+
+.newSize{
+  position: absolute;
+  top: 30%;
+  font-size: 22px;
+}
+
+.editor{
+  position: absolute;
+    left: 10%;
+    width: 20%;
+    top: 20%;
+}
+
+.noMore{
+  position: absolute;
+  color: #B8BED9;
+  left: 20%;
+  top: 200%;
+}
+
+.p3{
+  position: absolute;
+    width: 100%;
+    top: 45%;
+    /* left: 30%; */
+    display: flex;
+}
+
+.confirm{
+  position: absolute;
+    left: 10%;
+}
+  
+
+
+.cancel{
+  position: absolute;
+    /* right: 0%; */
+    left: 20%;
+}
+
+</style>
--- client/src/views/stat/index.vue
+++ client/src/views/stat/index.vue
@@ -1,7 +1,297 @@
 <template>
-  <div class="statContainer">Stat</div>
+  <div class="statContainer">
+    <div class="s1">
+      
+      <div class="sellWeek">
+        <Thunder class="thunder" />
+        <h3 class="tb">
+          TotalBuy
+        </h3>
+
+        <span class="s">K</span>
+        <h5 class="inWeek">
+          in week
+        </h5>
+      </div>
+
+      <div class="buyWeek">
+        <Thunder class="thunder" />
+        <h3 class="ts">
+          TotalSell
+        </h3>
+
+        <span class="s">K</span>
+
+        <h5 class="SellInWeek">
+          in week
+        </h5>
+
+      </div>
+    </div>
+
+    <div class="s2">
+      <div class="buyStatic">
+        <div class="img1">
+          <img src="../../assets/images/Activity.png" alt="">
+        </div>
+
+        <!-- <div class="view">
+          <div id="myEcharts" :style="{ width: '900px', height: '300px' }"></div>
+        </div> -->
+
+        <div class="contentB">WeeklyBuy</div>
+          
+      </div>
+    </div>
+
+    <div class="s3">
+      <div class="sellStatic">
+        <div class="img2">
+          <img src="../../assets/images/Activity.png" alt="">
+        </div>
+
+        <!-- <div class="view">
+          <div id="myEcharts" :style="{ width: '900px', height: '300px' }"></div>
+        </div> -->
+
+        <div class="contentS">WeeklySell</div>
+          
+      
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Thunder from '../../components/svgs/thunder.vue'
+import * as echarts from "echarts";
+// export default {
+//   name: "echartsBox",
+//   setup() {
+//     /// 声明定义一下echart
+//     let echart = echarts;
 
-<style scoped lang="less"></style>
+//     onMounted(() => {
+//       initChart();
+//     });
+
+//     onUnmounted(() => {
+//       echart.dispose;
+//     });
+	
+//     // 基础配置一下Echarts
+//     function initChart() {
+//       let chart = echart.init(document.getElementById("myEcharts"), "dark");
+//       // 把配置和数据放这里
+//       chart.setOption({
+//         xAxis: {
+//           type: "category",
+//           data: [
+//             "一月",
+//             "二月",
+//             "三月",
+//             "四月",
+//             "五月",
+//             "六月",
+//             "七月",
+//             "八月",
+//             "九月",
+//             "十月",
+//             "十一月",
+//             "十二月"
+//           ]
+//         },
+//         tooltip: {
+//           trigger: "axis"
+//         },
+//         yAxis: {
+//           type: "value"
+//         },
+//         series: [
+//           {
+//             data: [
+//               820,
+//               932,
+//               901,
+//               934,
+//               1290,
+//               1330,
+//               1320,
+//               801,
+//               102,
+//               230,
+//               4321,
+//               4129
+//             ],
+//             type: "line",
+//             smooth: true
+//           }
+//         ]
+//       });
+//       window.onresize = function() {
+//         //自适应大小
+//         chart.resize();
+//       };
+//     }
+
+//     return { initChart };
+//   }
+// };
+
+
+
+
+
+</script>
+
+<style scoped lang="less">
+.s1{
+  position: relative;
+}
+
+.s2{
+  position: relative;
+}
+
+.s3{
+  position: relative;
+}
+.chart{
+  height: 400px;
+}
+
+.sellWeek{
+   .hoverShadow ();
+  position: relative;
+  font-family: 'Abel';
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 328px;
+  height: 93px;
+  left: 200px;
+  top: 125px;
+  background: #ffffff;
+  box-shadow: 0px 0px 25px rgba(48, 73, 191, 0.07);
+  border-radius: 16px;
+}
+
+.tb{
+  position: relative;
+    right: 10%;
+    top: -8%;
+}
+
+.inWeek{
+  
+  position: relative;
+    /* bottom: 2px; */
+    /* right: 2px; */
+  right: 70%;
+  bottom: -10%;
+  color: #919EAB;
+}
+
+.buyWeek{
+   .hoverShadow ();
+  position: relative;
+  font-family: 'Abel';
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 328px;
+  height: 93px;
+  left: 624px;
+  top: 30px;
+  background: #ffffff;
+  box-shadow: 0px 0px 25px rgba(48, 73, 191, 0.07);
+  border-radius: 16px;
+}
+
+.SellInWeek{
+  color: #919EAB;
+    position: relative;
+    left: -70%;
+    bottom: -11%;
+}
+
+.ts{
+  position: relative;
+    left: -10%;
+    top: -5%;
+}
+
+.buyStatic{
+  .hoverShadow ();
+  position: relative;
+  font-family: 'Abel';
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 719px;
+  height: 320px;
+  left: 200px;
+  top: 80px;
+  background: #ffffff;
+  box-shadow: 0px 0px 25px rgba(48, 73, 191, 0.07);
+  border-radius: 16px;
+}
+
+.view{
+  position: relative;
+  width: 150px;
+  height: 150px;
+}
+
+.sellStatic{
+  .hoverShadow ();
+  position: relative;
+  font-family: 'Abel';
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 719px;
+  height: 320px;
+  left: 200px;
+  top: 160px;
+  background: #ffffff;
+  box-shadow: 0px 0px 25px rgba(48, 73, 191, 0.07);
+  border-radius: 16px;
+}
+
+.contentS{
+
+  position: relative;
+  left: -80%;
+  top: -30%;
+  font-family: Abel;
+  font-style: Regular;
+  font-size: 20px;
+  line-height: 25px;
+  line-height: 100%;
+  
+}
+
+.contentB{
+  position: relative;
+  left: -80%;
+  top: -30%;
+  font-family: Abel;
+  font-style: Regular;
+  font-size: 20px;
+  line-height: 25px;
+  line-height: 100%;
+}
+
+.img1{
+  position: relative;
+  top: -20%;
+  left: 5%;
+}
+
+.img2{
+  position: relative;
+  top: -20%;
+  left: 5%;
+}
+</style>
