ebd5256ef86c87c60ce8e627bdf26e3edc01269f
Liya-Xie <112081318+Liya-Xie@users.noreply.github.com>
2022-10-19 19:44:25 +1100

    Update MainSystem.sol

--- truffle/contracts/MainSystem.sol
+++ truffle/contracts/MainSystem.sol
@@ -58,6 +58,11 @@ contract MainSystem is BeanStructs {
     function returnPurchasePostResponseMessagesByKey(uint _postKey) public view returns(PostResponseMessage[] memory){
         return buyerService.returnPurchasePostResponseMessagesByKey(_postKey);
     }
+    
+    // return recommended price of electricity unit price for buy post
+    function returnRecentAveragePriceforBuy() public view returns (uint) {
+        return buyerService.returnRecentAveragePriceforBuy();
+    }
 
     // ------------------------- seller service API -------------------------
 
