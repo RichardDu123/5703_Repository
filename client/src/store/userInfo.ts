541184b20e0626d4ec7a7934f06fef793ed47c8b
RichardDu123 <yudu9851@uni.sydney.edu.au>
2022-11-11 21:52:06 +1100

    sourcemap

--- client/src/store/userInfo.ts
+++ client/src/store/userInfo.ts
@@ -82,14 +82,11 @@ export const useUserStore = defineStore('User', {
       this.relies = keys
     },
     async setRecnetTransaction() {
-      console.log('1')
       this.recentTransaction = []
       const ETHStore = useETHStore()
       const contract = ETHStore.contract as Contract
       const address = ETHStore.accounts ? ETHStore.accounts[0] : ''
-      console.log('2')
       const res = await returnRecentTransactions(contract, address)
-      console.log('3')
       res.forEach((item: any) => {
         this.recentTransaction.push({
           time: item.createdAt,
@@ -98,7 +95,6 @@ export const useUserStore = defineStore('User', {
         })
       })
       console.log(this.recentTransaction)
-      console.log(res)
     },
   },
 })
