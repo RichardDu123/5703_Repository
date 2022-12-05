8fbbcfea21b56889d46339b8c30f3f5189ac960c
RichardDu123 <yudu9851@uni.sydney.edu.au>
2022-12-05 12:38:58 +1100

    optimize webpack

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,9 +2,12 @@ const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 const path = require('path')
-
+let productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
 module.exports = {
-  productionSourceMap: true,
+  productionSourceMap: false,
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'assets',
   configureWebpack: {
     plugins: [
       AutoImport({
@@ -15,7 +18,73 @@ module.exports = {
       }),
     ],
   },
+  //webpack配置
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+      config.mode = 'production'
+      // 将每个依赖包打包成单独的js文件
+      let optimization = {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 20000,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name (module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`
+              }
+            }
+          }
+        },
 
+        minimizer: [
+        ]
+      }
+
+      //配置hash
+      Object.assign(config, {
+        optimization,
+        output: {
+          ...config.output,
+          filename: `static/js/[name].[contenthash:5].js`,
+          chunkFilename: `static/js/[name].[contenthash:5].js`
+        },
+        // 插件配置
+        plugins: [
+          ...config.plugins,
+        ]
+      })
+    } else {
+      // 为开发环境修改配置...
+      config.mode = 'development'
+    }
+
+  },
+  chainWebpack: config => {
+    if (process.env.use_analyzer) { // 添加分析工具
+      config
+        .plugin('webpack-bundle-analyzer')
+        .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+    }
+    //gzip压缩
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin("CompressionPlugin").use('compression-webpack-plugin', [{
+        filename: '[path][base].gz',
+        algorithm: 'gzip',
+        test: productionGzipExtensions,
+        threshold: 10240,
+        minRatio: 0.8
+      }])
+    }
+  },
+  //引入less全局变量
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
